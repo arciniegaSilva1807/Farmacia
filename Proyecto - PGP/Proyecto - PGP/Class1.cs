7977cce07a6809a3318ff249e564ce095d075194
alien trümper <samobojcow_alien@hotmail.com>
2020-06-04 16:09:46 -0500

    Cambios Ahlai Gitignore

---
+++ Proyecto - PGP/Proyecto - PGP/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto___PGP
+{
+    class Class1
+    {
+        //dcscsdc
+    }
+}
